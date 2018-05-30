d16644f97c248073af15b39ff6472de1676449ac
Vlad <vlad.radin@gmail.com>
2018-05-30 07:53:00 +0300

    Added ElasticSearch Mappings

--- ElasticSearchApp/Program.cs
+++ ElasticSearchApp/Program.cs
@@ -1,7 +1,10 @@
-﻿using Nest;
+﻿using Elasticsearch.Net;
+using Nest;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System;
 using System.ComponentModel;
+using System.Linq.Expressions;
 
 
 
@@ -31,6 +34,25 @@ public enum MaritalStatus
     Divorced
 }
 
+public class Personal
+{
+    public string Name { get; set; }
+
+    public Gender Gender { get; set; }
+
+    public uint Age { get; set; }
+
+    public string City { get; set; }
+
+    public string Email { get; set; }
+
+    public string Phone { get; set; }
+
+    public string State { get; set; }
+
+    public string Street { get; set; }
+}
+
 public class Employee
 {
     public string FirstName { get; set; }
@@ -44,13 +66,13 @@ public class Employee
     public string Address { get; set; }
 
     [Text]
-    [TypeConverter(typeof(StringEnumConverter))]
+    [JsonConverter(typeof(StringEnumConverter))]
     public Gender Gender { get; set; }
 
     public int Age { get; set; }
 
     [Text]
-    [TypeConverter(typeof(StringEnumConverter))]
+    [JsonConverter(typeof(StringEnumConverter))]
     public MaritalStatus MaritalStatus { get; set; }
 
     public string Interests { get; set; }
@@ -58,21 +80,101 @@ public class Employee
 
 namespace ElasticSearchApp
 {
+
+    public static class EsExtensions
+    {
+        public static PropertiesDescriptor<T> CreateTextFieldWithKeyword<T>(this PropertiesDescriptor<T> textDescriptor, Expression<Func<T, object>> fieldPath)
+            where T : class
+        {
+            return textDescriptor.Text(nameDesc => nameDesc
+                                     .Name(fieldPath)
+                                     .Fields(f => f
+                                         .Keyword(pk => pk
+                                             .Name("keyword")
+                                             .Normalizer("removeNumbers")
+                                             .IgnoreAbove(256))));
+        }
+
+        public static PropertiesDescriptor<T> CreateNumber<T>(this PropertiesDescriptor<T> textDescriptor, Expression<Func<T, object>> fieldPath)
+                 where T : class
+        {
+            return textDescriptor.Number(nameDesc => nameDesc
+                                     .Name(fieldPath)
+                                     .Type(NumberType.Long));
+        }
+
+        public static PropertiesDescriptor<T> CreateNumber2<T>(this PropertiesDescriptor<T> textDescriptor, Expression<Func<T, object>> fieldPath)
+         where T : class
+        {
+            return textDescriptor.Number(nameDesc => nameDesc
+                                     .Name(fieldPath)
+                                     .Type(NumberType.Long));
+        }
+    }
     class Program
     {
+
+        public static TypeMappingDescriptor<Personal> MapPersonal(TypeMappingDescriptor<Personal> personalDescriptor)
+        {
+            return personalDescriptor.Properties(persProps => persProps
+                                .CreateTextFieldWithKeyword(pers => pers.Name)
+                                .CreateTextFieldWithKeyword(pers => pers.Gender)
+                                .CreateNumber(pers => pers.Age)
+                                .CreateTextFieldWithKeyword(pers => pers.Email)
+                                .CreateTextFieldWithKeyword(pers => pers.Phone)
+                                .CreateTextFieldWithKeyword(pers => pers.Street)
+                                .CreateTextFieldWithKeyword(pers => pers.City)
+                                .CreateTextFieldWithKeyword(pers => pers.State));
+        }
+
         static void Main(string[] args)
         {
-            var client = new ElasticClient(new Uri("http://localhost:9200"));
+            var connectionPool = new SingleNodeConnectionPool(new Uri("http://localhost:9200"));
 
-            if (client.IndexExists("companyDatabase").Exists)
-                client.DeleteIndex("companyDatabase");
+            var settings = new ConnectionSettings(connectionPool)
+                .DisableDirectStreaming();
 
-            var rsp = client.CreateIndex("companyDatabase", i => i
+            var client = new ElasticClient(settings);
+
+            const string custoemrs = "customers";
+
+
+            //if (client.IndexExists("companyDatabase").Exists)
+            //    client.DeleteIndex("companyDatabase");
+
+            //var rsp = client.CreateIndex("companyDatabase", i => i
+            //                  .Mappings(mp => mp
+            //                    .Map<Employee>(emp => emp
+            //                        .AutoMap())));
+
+            var rsp = client.CreateIndex(custoemrs, i => i
+                              .Settings(sett => sett
+                                .Analysis(an => an
+                                    .Normalizers(norm => norm
+                                        .Custom(custoemrs, cstNorm => cstNorm
+                                             .CharFilters("removeNumbers")))
+                                .CharFilters(cFilter => cFilter
+                                   .Mapping("removeNumbers", mpf => mpf
+                                        .Mappings("1=>","2=>","3=>","4=>","5=>","6=>","7=>","8=>","9=>")))))
                               .Mappings(mp => mp
-                                .Map<Employee>(emp => emp
-                                    .AutoMap())));
+                                .Map<Personal>(pers => MapPersonal(pers))));
 
-                                          
+            var result = client.Search<Personal>(s => s
+                                   .Index(custoemrs)
+                                   .Type("personal")
+                                   .Size(50)
+                                   .Query(q => q
+                                        .MatchAll())
+                                        .Aggregations(ag => ag
+                                            .Terms("state", stateAgg => stateAgg
+                                                .Field("state.keyword")
+                                                .Size(40))));
+
+            var stateAggs = result.Aggs.Terms("state");
+
+
+            var resCount = result.Documents;
+
         }
     }
 }
